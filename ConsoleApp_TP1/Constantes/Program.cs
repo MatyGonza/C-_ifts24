91c5a64f7149639a375fae1d0ca0940ba617cca5
matigonza <gonzalezmatias879@gmail.com>
2025-05-31 21:50:54 -0300

    primer commit

---
+++ ConsoleApp_TP1/Constantes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Operadores
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int Var1 = 10;
+            int Var2 = 100;
+            bool Var3 = true;
+            bool Var4 = false;
+            int varResultado = 0;
+            
+            Console.WriteLine("Operadores aritmeticos"); 
+            Console.WriteLine();
+            
+            Console.WriteLine("var1 = {0}",Var1);
+            Console.WriteLine("var2 = {0}",Var2);
+            Console.WriteLine();
+            
+            Console.WriteLine("var1 + var2 = {0}", Var1 + Var2 );
+            Console.WriteLine("var1 - var2 = {0}", Var1 - Var2 );
+            Console.WriteLine("var1 / var2 = {0}", Var1 / Var2 );
+            Console.WriteLine("var1 % var2 = {0}", Var1 % Var2 );
+        }
+    }
+}
---
+++ ConsoleApp_TP1/HolaMundo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class  Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine();
+            Console.Write("pulse una tecla para continuar");
+            Console.ReadKey(true);
+        }
+    }
+}
